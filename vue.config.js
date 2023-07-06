dd004961e7afddb3450d779fda13101fce4eb9ac
Brant Mondy <33537571+tdh1999@users.noreply.github.com>
2023-07-06 09:24:31 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+	lintOnSave:false,
+	publicPath: "/apps/uca/",
+	productionSourceMap:false,
+	devServer: {
+		open: true,
+		host: 'localhost',
+		port: 8000,
+		https: false,
+		//以上的ip和端口是我们本机的;下面为需要跨域的
+		proxy: {//配置跨域
+			'/api': {
+				target: 'http://1.15.80.51',//这里后台的地址模拟的;应该填写你们真实的后台接口
+				ws: true,
+				changOrigin: true,//允许跨域
+				pathRewrite: {
+					'^/api': ''//请求的时候使用这个api就可以
+				}
+			}
+
+		}
+	}
+};
